011ea527342d02432980f7a1c0a0919f1ac48d95
rguichard <rguichard@vsense.fr>
2016-03-06 18:28:29 +0100

    replace vsense git repo by the osones one

--- revealjs-builder/build-docker.sh
+++ revealjs-builder/build-docker.sh
@@ -11,6 +11,6 @@ done
 pandoc slide -t revealjs -f markdown -s -o ../../docker-s3/index.html --slide-level 3 -V theme=solarized -V navigation=frame -V revealjs-url=.
 head ../../docker-s3/index.html
 
-git clone https://github.com/vsense/reveal.js revealjs
+git clone https://github.com/osones/reveal.js revealjs
 cp -v /formations/docker-s3/index.html /formations/latex/docker/revealjs/index.html
 ls revealjs/
