d49178f1986bf0decd456b568de2fb2c6f02518e
rguichard <rguichard@vsense.fr>
2016-03-06 18:58:56 +0100

    use osones branch for revealjs repository

--- revealjs-builder/build-docker.sh
+++ revealjs-builder/build-docker.sh
@@ -8,9 +8,11 @@ cd latex/docker
 for i in $(ls -I README.md | grep .md); do
       cat $i >> slide
 done
-pandoc slide -t revealjs -f markdown -s -o ../../docker-s3/index.html --slide-level 3 -V theme=solarized -V navigation=frame -V revealjs-url=.
+pandoc slide -t revealjs -f markdown -s -o ../../docker-s3/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=.
 head ../../docker-s3/index.html
 
 git clone https://github.com/osones/reveal.js revealjs
+cd revealjs
+git checkout osones
 cp -v /formations/docker-s3/index.html /formations/latex/docker/revealjs/index.html
 ls revealjs/
