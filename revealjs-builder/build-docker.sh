a463487767230746bec422d2df61d4e6e0e5b9be
rguichard <rguichard@vsense.fr>
2016-03-07 11:10:58 +0100

    add openstack build

--- revealjs-builder/build-docker.sh
+++ revealjs-builder/build-docker.sh
@@ -15,3 +15,15 @@ git clone https://github.com/osones/reveal.js revealjs
 cd revealjs
 git checkout osones
 cp -v /formations/docker-s3/index.html /formations/latex/docker/revealjs/index.html
+
+# OpenStack
+# Only testing for now
+
+cd /formations
+mkdir -v openstack-html-s3
+cd latex/openstack
+pandoc slide -t revealjs -f markdown -s -o ../../openstack-html-s3/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=.
+git clone https://github.com/osones/reveal.js revealjs
+cd revealjs
+git checkout osones
+cp -v /formations/openstack-html-s3/index.html /formations/latex/openstack/revealjs/index.html
