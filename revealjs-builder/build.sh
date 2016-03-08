4794f65a7fb34c76513c1c2a46890035be67c0c1
rguichard <rguichard@vsense.fr>
2016-03-08 11:36:14 +0100

    replace 'latex' dir by 'cours' dir

--- revealjs-builder/build.sh
+++ revealjs-builder/build.sh
@@ -4,7 +4,7 @@ cd /formations
 
 mkdir -v docker-s3
 
-cd latex/docker
+cd cours/docker
 for i in $(ls -I README.md | grep .md); do
       cat $i >> slide
 done
@@ -14,18 +14,18 @@ head ../../docker-s3/index.html
 git clone https://github.com/osones/reveal.js revealjs
 cd revealjs
 git checkout osones
-cp -v /formations/docker-s3/index.html /formations/latex/docker/revealjs/index.html
-cp -rv /formations/images /formations/latex/docker/revealjs/
+cp -v /formations/docker-s3/index.html /formations/cours/docker/revealjs/index.html
+cp -rv /formations/images /formations/cours/docker/revealjs/
 
 # OpenStack
 # Only testing for now
 
 cd /formations
 mkdir -v openstack-html-s3
-cd latex/openstack
+cd cours/openstack
 pandoc slide -t revealjs -f markdown -s -o ../../openstack-html-s3/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=.
 git clone https://github.com/osones/reveal.js revealjs
 cd revealjs
 git checkout osones
-cp -v /formations/openstack-html-s3/index.html /formations/latex/openstack/revealjs/index.html
-cp -rv /formations/images /formations/latex/openstack/revealjs/
+cp -v /formations/openstack-html-s3/index.html /formations/cours/openstack/revealjs/index.html
+cp -rv /formations/images /formations/cours/openstack/revealjs/
