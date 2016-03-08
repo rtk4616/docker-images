f4864c197e066c9bf15f2abfff5fe2a9ad9d2ee7
ArchiFleKs <klefevre@vsense.fr>
2016-03-08 15:36:31 +0100

    use official revealjs repo and add osones theme

--- revealjs-builder/build.sh
+++ revealjs-builder/build.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+
+# Docker
 
 cd /formations/cours/docker
-mkdir -v output-html
-for i in $(ls -I README.md | grep .md); do
+
+mkdir -vp output-html
+
+for i in [0-9]*.md; do
       cat $i >> slide
 done
+
 pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="Docker formation"
-git clone https://github.com/osones/reveal.js revealjs
-cd revealjs
-git checkout osones
+
+git clone https://github.com/hakimel/reveal.js.git revealjs
+
 cp -v /formations/cours/docker/output-html/index.html /formations/cours/docker/revealjs/index.html
 cp -rv /formations/images /formations/cours/docker/revealjs/
+cp -rv /formation/cours/styles/osones.css /formation/cours/docker/revealsjs/css/theme/osones.css
+
 
 # OpenStack
-# Only testing for now
 
 cd /formations/cours/openstack
-mkdir -v output-html
+
+mkdir -vp output-html
+
 pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="OpenStack formation"
-git clone https://github.com/osones/reveal.js revealjs
-cd revealjs
-git checkout osones
+
+git clone https://github.com/hakimel/reveal.js.git revealjs
+
 cp -v /formations/cours/openstack/output-html/index.html /formations/cours/openstack/revealjs/index.html
 cp -rv /formations/images /formations/cours/openstack/revealjs/
+cp -rv /formation/cours/styles/osones.css /formation/cours/docker/revealsjs/css/theme/osones.css
