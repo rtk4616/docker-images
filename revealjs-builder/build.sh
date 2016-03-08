07037500beb20e913577edc37c3ce36b16a03e66
rguichard <rguichard@vsense.fr>
2016-03-08 20:17:45 +0100

    refactor + looping

--- revealjs-builder/build.sh
+++ revealjs-builder/build.sh
@@ -1,36 +1,20 @@
 #!/bin/bash -xe
 
+COURS_DIR=/formations/cours
+IMG_DIR=/formations/images
 
-# Docker
-
-cd /formations/cours/docker
-
-mkdir -vp output-html
-
-for i in [0-9]*.md; do
-      cat $i >> slide
+for cours in docker openstack; do
+    cd /formations/cours/$cours
+    mkdir -vp output-html
+    if [[ $cours = "docker" ]]; then
+        for i in [0-9]*.md; do
+            cat $i >> slide
+        done
+    fi
+    pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="$cours formation"
+    git clone https://github.com/hakimel/reveal.js.git revealjs
+    cp  $COURS_DIR/$cours/output-html/index.html $COURS_DIR/$cours/revealjs/index.html
+    cp -r $IMG_DIR/$COURS_DIR/$cours/revealjs/
+    cp $COURS_DIR/styles/osones.css $COURS_DIR/$cours/revealjs/css/theme/osones.css
+    cp -r $COURS_DIR/$cours/revealjs/css $COURS_DIR/$cours/output-html/
 done
-
-pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="Docker formation"
-
-git clone https://github.com/hakimel/reveal.js.git revealjs
-
-cp  /formations/cours/docker/output-html/index.html /formations/cours/docker/revealjs/index.html
-cp -r /formations/images /formations/cours/docker/revealjs/
-cp /formations/cours/styles/osones.css /formations/cours/docker/revealjs/css/theme/osones.css
-cp -r /formations/cours/docker/revealjs/css /formations/cours/docker/output-html/
-
-# OpenStack
-
-cd /formations/cours/openstack
-
-mkdir -vp output-html
-
-pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="OpenStack formation"
-
-git clone https://github.com/hakimel/reveal.js.git revealjs
-
-cp /formations/cours/openstack/output-html/index.html /formations/cours/openstack/revealjs/index.html
-cp -r /formations/images /formations/cours/openstack/revealjs/
-cp /formations/cours/styles/osones.css /formations/cours/docker/revealjs/css/theme/osones.css
-cp -r /formations/cours/openstack/revealjs/css /formations/cours/openstack/output-html/
