a2943c5f3388092e804d5044af2d4d52c37e3b9e
rguichard <rguichard@vsense.fr>
2016-03-08 14:46:34 +0100

    fix Makefile path

--- latex-builder/build.sh
+++ latex-builder/build.sh
@@ -2,5 +2,6 @@
 
 cd /formations/cours/openstack
 mkdir -v output-pdf
+cd /formations
 make course=openstack
-mv -v result/*.pdf output-pdf/
+mv -v result/*.pdf /formations/cours/openstack/output-pdf/
