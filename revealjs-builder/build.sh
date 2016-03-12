92e38f31f0b0891b99363a93a9c1c32549f97f5a
rguichard <rguichard@vsense.fr>
2016-03-12 16:20:39 +0100

    add modularity to the build system, a course list defines which modules are part of which course

--- revealjs-builder/build.sh
+++ revealjs-builder/build.sh
@@ -2,19 +2,21 @@
 
 COURS_DIR=/formations/cours
 IMG_DIR=/formations/images
+LIST=/formations/cours/list.md
 
-for cours in docker openstack; do
-    cd /formations/cours/$cours
-    mkdir -vp output-html
-    if [[ $cours = "docker" ]]; then
-        for i in [0-9]*.md; do
-            cat $i >> slide
-        done
-    fi
-    pandoc slide -t revealjs -f markdown -s -o output-html/index.html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. --title-prefix="$cours formation"
-    git clone https://github.com/hakimel/reveal.js.git revealjs
-    cp  $COURS_DIR/$cours/output-html/index.html $COURS_DIR/$cours/revealjs/index.html
-    cp -r $IMG_DIR $COURS_DIR/$cours/revealjs/
-    cp $COURS_DIR/styles/osones.css $COURS_DIR/$cours/revealjs/css/theme/osones.css
-    cp -r $COURS_DIR/$cours/revealjs/css $COURS_DIR/$cours/output-html/
-done
+git clone https://github.com/hakimel/reveal.js.git $COURS_DIR/revealjs
+
+while line; do
+    cours=$(echo $line | cut -d; -f1)
+    modules=$(echo $line | cut -d; -f2)
+    mkdir $COURS_DIR/output-html
+    for module in $(echo $modules); do
+        cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
+    done
+    pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=.
+done < $LIST
+
+cp  $COURS_DIR/output-html/*.html $COURS_DIR/revealjs/
+cp -r $IMG_DIR $COURS_DIR/revealjs/
+cp $COURS_DIR/styles/osones.css $COURS_DIR/revealjs/css/theme/osones.css
+cp -r $COURS_DIR/revealjs/css $COURS_DIR/output-html/
