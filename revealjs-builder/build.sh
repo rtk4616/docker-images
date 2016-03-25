58ee5c25520149ffdd3d66e0251a5bb9b27d1d67
rguichard <rguichard@vsense.fr>
2016-03-25 15:07:22 +0100

    use absolute link for css and js files, the clone of revealjs git repository is no longer necessary

--- revealjs-builder/build.sh
+++ revealjs-builder/build.sh
@@ -4,22 +4,15 @@ COURS_DIR=/formations/cours
 IMG_DIR=/formations/images
 LIST=/formations/cours/list.md
 
-git clone https://github.com/hakimel/reveal.js.git $COURS_DIR/revealjs
-mkdir $COURS_DIR/output-html
+mkdir -p $COURS_DIR/output-html/revealjs/css/theme
 
 while IFS=: read cours modules; do
     for module in $modules; do
         cat $COURS_DIR/$module >> $COURS_DIR/slide-$cours
     done
-    pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url=. -V slideNumber="true"
+    pandoc $COURS_DIR/slide-$cours -t revealjs -f markdown -s -o $COURS_DIR/output-html/"$cours".html --slide-level 3 -V theme=osones -V navigation=frame -V revealjs-url="http://formation.osones.com/revealjs" -V slideNumber="true"
 done < $LIST
 
-# cp into revealjs/ for web browser visualization or pdf generation
-cp  $COURS_DIR/output-html/*.html $COURS_DIR/revealjs/
-cp -r $IMG_DIR $COURS_DIR/revealjs/
-cp $COURS_DIR/styles/osones.css $COURS_DIR/revealjs/css/theme/osones.css
-
 # cp into output-html/ for push to S3
-cp -r $COURS_DIR/revealjs/css $COURS_DIR/output-html/
+cp -r $COURS_DIR/styles/osones.css $COURS_DIR/output-html/revealjs/css/theme
 cp -r $IMG_DIR $COURS_DIR/output-html/
-cp -r $COURS_DIR/revealjs/css $COURS_DIR/output-html/
